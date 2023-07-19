16d022ce94f7153162dc9b7b97dc0f51fc7e147b
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-07-19 23:53:51 +0900

    gin で Hello World ができた。

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Server Start")
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
+	err := router.Run(":4456")
+	fmt.Println(err)
 }
